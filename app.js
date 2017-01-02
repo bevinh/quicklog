dd3d74d8b6a4ebc2d505cba97f03cc12608e5016
bevinh <bevin.hernandez@gmail.com>
2017-01-02 12:45:12 -0500

    added mocha and test dir, transformed module

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var fs = require('fs');
-var path = '';
 
-//write the function to log any errors to the file you specify in the path variable
-function logError(path, e){
-    var d = new Date();
-    var eWrite = "[" + d + " ]";
-    eWrite += ":" + e + "\n";
+module.exports = function (path, e){
+    var fs = require('fs');
+    //write the function to log any errors to the file you specify in the path variable
+    function quickLog(path, e) {
+        var d = new Date();
+        var eWrite = "[" + d + " ]";
+        eWrite += ":" + e + "\n";
 
-    fs.appendFileSync(path, eWrite, encoding='utf8');
-}
+        fs.appendFileSync(path, eWrite, encoding = 'utf8');
+    }
+    return quickLog(path, e);
+};
+
