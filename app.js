51488d9aa32d6c60599fb7429b947223c4e6a635
bevinh <bevin.hernandez@gmail.com>
2017-01-02 12:02:26 -0500

    added quickscript to log

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 var fs = require('fs');
+var path = '';
+
+//write the function to log any errors to the file you specify in the path variable
+function logError(path, e){
+    var d = new Date();
+    var eWrite = "[" + d + " ]";
+    eWrite += ":" + e + "\n";
+
+    fs.appendFileSync(path, eWrite, encoding='utf8');
+}
